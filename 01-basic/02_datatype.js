ae46437997c5513ef0e24055cdc04d139aa7cc0c
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-01-09 08:26:28 +0000

    datatype

---
+++ 01-basic/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) // we are using nodejs, not browser
+
+// anything can search go to TC39.es
+
+let name = "Rajab"
+let age = 18
+let isLoggedIn = false
+
+
+// number =>2 to power 53
+// bigint 
+// string => ""
+// bolean => true/false
+// null => standalone value 
+//undefined => 
+// symbol => unique 
+// object 
+
+
+console.log(typeof undefined) // output / undefined MCQs
+console.log(typeof null) // output/ object MCQs
