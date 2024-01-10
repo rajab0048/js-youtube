23341ea9e5efb43d11d557ca7874236b0d89c2b9
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-01-10 07:23:31 +0000

    comparison

---
+++ 01-basic/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/* don't use comparison between different datatype because they can convert null into NaN or 0.
+
+ console.log(null > 0);
+ console.log(null >= 0);
+ console.log(null == 0);
+
+ console.log(undefined > 0);
+ console.log(undefined >= 0);
+ console.log(undefined == 0);
+ */
+
+ // === (strict check )
+
+ console.log("2" === 2);
+
+ // === this comparison operator strictly check datatype and if match then give output 
