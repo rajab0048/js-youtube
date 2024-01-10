7955acc4348b44682268a55b16461f9fea578ef3
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-01-10 06:24:40 +0000

    Conversion of DataType

---
+++ 01-basic/03_datatypeconversion.js
@@ -0,0 +1,37 @@
+/*let score = "33abc"
+
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+*/
+// DataType conversion Rule
+// "33" => 33
+//"33abc" => NaN(not a number)
+// null => 0
+// undefined => NaN
+// true/false => 1/0
+
+
+//let isloggedIn = 1
+
+//let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn);
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Rajab" => true
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof someNumber);
+
