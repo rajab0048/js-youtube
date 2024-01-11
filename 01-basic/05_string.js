4c83322554d90b27b7820072d033428bf6c41f2d
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-01-11 08:10:00 +0000

    strings

---
+++ 01-basic/05_string.js
@@ -0,0 +1,21 @@
+const name = "Rajab "
+const repoCount = 1
+
+ 
+// console.log(name + repoCount ); // old way to add string
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// modern way to add string 
+
+
+const gameName = new String('Rajab') // new mehtod to write string value
+
+console.log(gameName[4]); // to watch key values of string
+console.log(gameName.__proto__); // to watch prototype
+
+// use some prototype
+
+console.log(gameName.length);
+
+// notes on register
+
