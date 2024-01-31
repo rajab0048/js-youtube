18ef0fe93a1a0d097cf6e56fedb03f1ca501d640
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-01-31 07:37:15 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+// Array 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[0])
+
+
+// Array mehtods
+
+myArr.push(6)
+console.log(mt);
