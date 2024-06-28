bbeb508bd14e24abeb7d9e2b2fdb3f0aa1aa623f
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-06-28 07:24:55 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuaray");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 2:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
