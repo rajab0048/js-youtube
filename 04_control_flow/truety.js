aeb37a78b49cd4637885f155d8c9408c5cc08b66
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-06-28 07:26:09 +0000

    truethy and falsy

---
+++ 04_control_flow/truety.js
@@ -0,0 +1,49 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+
+// falsy value
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN 
+
+// truthy
+
+// "0", 'false', " ", [], {}, funcation(){}
+
+// Array check
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+
+// Object checking
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+// terniary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
