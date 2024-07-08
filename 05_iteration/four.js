79bbfd08a0b4fc79dc5898e59ce42a8066c9bb3e
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-07-08 08:01:44 +0000

    for of loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,25 @@
+const myObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'rubby',
+    swift: 'Swift by apple'
+}
+
+for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United State of America")
+// map.set('PAK', "Pakistan")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
