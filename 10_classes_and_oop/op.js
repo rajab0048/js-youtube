03f1af51865561f60a1b08e87fede81748360504
rajab0048 <156059107+rajab0048@users.noreply.github.com>
2024-08-12 06:09:49 +0000

    OOP practise

---
+++ 10_classes_and_oop/op.js
@@ -0,0 +1,13 @@
+const user = {
+    username: "Rajab",
+    loginCount: "8",
+    siggnedIn: "true",
+
+    getUserDetails: function(){
+        console.log("ok")
+    }
+}
+
+// console.log(user.username)
+
+
